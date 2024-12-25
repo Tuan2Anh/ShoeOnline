6c766dcb913b5702377461961ec4296fcdbe0f0b
Nguyễn Tuấn Anh <157350887+Tuan2Anh@users.noreply.github.com>
2024-12-25 00:38:00 +0000

    UpdateBook

---
+++ bookOnline/resources/views/Book.blade.php
@@ -0,0 +1,14 @@
+
+<html>
+    <div>
+        @yfield("content");    
+    </div>
+    <thead>
+        <tr>
+            <th>Book</th>
+        </tr>
+    </thead>
+    <tbody>
+        {{$books}}
+    </tbody>
+</html>
